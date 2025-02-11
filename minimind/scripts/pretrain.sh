f6f4875c3a1f96db477d3e232d14283635f99368
XavierZXY <zxy_xavier@163.com>
2025-02-11 19:56:52 +0800

    feat: :sparkles: Add pretraining script and training logic; update .gitignore to include wandb directory

--- minimind/scripts/pretrain.sh
+++ minimind/scripts/pretrain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ../
 source ../.venv/bin/activate
-CUDA_VISIBLE_DEVICES=7 python train_pretrain.py --data_path /home/zxy/.cache/modelscope/hub/datasets/gongjy/minimind-dataset/pretrain_hq.jsonl
+# CUDA_VISIBLE_DEVICES=7 python train_pretrain.py --data_path /home/zxy/.cache/modelscope/hub/datasets/gongjy/minimind-dataset/pretrain_hq.jsonl --use_moe True
+torchrun --nproc_per_node 8 train_pretrain.py --data_path /home/zxy/.cache/modelscope/hub/datasets/gongjy/minimind-dataset/pretrain_hq.jsonl --use_moe True --use_wandb
