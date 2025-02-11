98d42d470515326175d6f544663384b3274d2dd5
XavierZXY <zxy_xavier@163.com>
2025-02-11 14:32:20 +0800

    feat: :sparkles: Implement MoEGate and MOEFeedForward classes for mixture of experts architecture

--- minimind/model/model.py
+++ minimind/model/model.py
@@ -197,7 +197,7 @@ class MoEGate(nn.Module):
         self.scoring_func = config.scoring_func
         self.alpha = config.aux_loss_alpha  # 设置辅助损失的权重
         self.seq_aux = config.seq_aux
-        self.norm_topk_prob = config.norm_topk_prob
+        self.norm_topk_prob = config.norm_topk_prob  # 是否归一化topk概率
         self.gating_dim = config.dim  # 设置门控维度
         self.weight = nn.Parameter(
             torch.empty((self.n_routed_experts, self.gating_dim))
@@ -208,3 +208,328 @@ class MoEGate(nn.Module):
         import torch.nn.init as init
 
         init.kaiming_normal_(self.weight, a=math.sqrt(5))  # 使用kaiming初始化
+
+    def forward(self, hidden_states):
+        bsz, seq_len, h = hidden_states.shape
+        hidden_states = hidden_states.view(-1, h)
+        logits = F.linear(hidden_states, self.weight, None)
+        if self.scoring_func == "softmax":
+            scores = logits.softmax(dim=-1)
+        else:
+            raise NotImplementedError(
+                f"insupportable scoring function for MoE gating: {self.scoring_func}"
+            )
+
+        topk_weight, topk_idx = torch.topk(
+            scores, k=self.top_k, dim=-1, sorted=False
+        )  # 获取topk的权重和索引
+
+        if self.top_k > 1 and self.norm_topk_prob:
+            denominator = topk_weight.sum(dim=-1, keepdim=True) + 1e-20
+            topk_weight = topk_weight / denominator  # 归一化topk概率
+
+        if self.training and self.alpha > 0.0:
+            # 计算一个辅助损失，用于在训练过程中平衡不同专家的使用。这个辅助损失基于专家评分和 topk 选择的索引，
+            # 通过不同的计算方式来调整专家的使用频率。
+            scores_for_aux = scores
+            aux_topk = self.top_k
+            topk_idx_for_aux_loss = topk_idx.view(bsz, -1)
+            if self.seq_aux:
+                scores_for_seq_aux = scores_for_aux.view(bsz, seq_len, -1)
+                ce = torch.zeros(
+                    bsz, self.n_routed_experts, device=hidden_states.device
+                )
+                ce.scatter_add_(
+                    1,
+                    topk_idx_for_aux_loss,
+                    torch.ones(
+                        bsz, seq_len * aux_topk, device=hidden_states.device
+                    ),
+                ).div_(seq_len * aux_topk / self.n_routed_experts)
+                aux_loss = (ce * scores_for_seq_aux.mean(dim=1)).sum(
+                    dim=1
+                ).mean() * self.alpha
+            else:
+                mask_ce = F.one_hot(
+                    topk_idx_for_aux_loss.view(-1),
+                    num_classes=self.n_routed_experts,
+                )
+                ce = mask_ce.float().mean(0)
+                Pi = scores_for_aux.mean(0)
+                fi = ce * self.n_routed_experts
+                aux_loss = (Pi * fi).sum() * self.alpha
+        else:
+            aux_loss = 0.0
+
+        return topk_idx, topk_weight, aux_loss
+
+
+class MOEFeedForward(nn.Module):
+    def __init__(self, config: LMConfig):
+        super().__init__()
+        self.config = config
+        self.experts = nn.ModuleList(
+            [FeedForward(config) for _ in range(config.n_routed_experts)]
+        )
+        self.gate = MoEGate(config)
+        if config.n_shared_experts is not None:
+            self.shared_experts = FeedForward(config)
+
+    def forward(self, x):
+        identity = x
+        orig_shape = x.shape
+        bsz, seq_len, _ = x.shape
+        # 通过门控选择专家
+        topk_idx, topk_weight, aux_loss = self.gate(x)
+        x = x.view(-1, x.shape[-1])
+        flat_topk_idx = topk_idx.view(-1)
+        if self.training:
+            # TODO more learning
+            # 训练模式下，重复输入数据
+            x = x.repeat_interleave(self.config.num_experts_per_tok, dim=0)
+            y = torch.empty_like(x, dtype=torch.float16)
+            for i, expert in enumerate(self.experts):
+                y[flat_topk_idx == i] = expert(x[flat_topk_idx == i]).to(
+                    y.dtype
+                )  # 确保类型一致
+            y = (y.view(*topk_weight.shape, -1) * topk_weight.unsqueeze(-1)).sum(
+                dim=1
+            )
+            y = y.view(*orig_shape)
+        else:
+            # 推理模式下，只选择一个专家
+            y = self.moe_infer(x, flat_topk_idx, topk_weight.view(-1, 1)).view(
+                *orig_shape
+            )
+        if self.config.n_shared_experts is not None:
+            y = y + self.shared_experts(identity)
+        self.aux_loss = aux_loss
+        return y
+
+    @torch.no_grad()
+    def moe_infer(self, x, flat_expert_indices, flat_expert_weights):
+        expert_cache = torch.zeros_like(x)
+        idxs = flat_expert_indices.argsort()
+        tokens_per_expert = (
+            flat_expert_indices.bincount().cpu().numpy().cumsum(0)
+        )
+        token_idxs = idxs // self.config.num_experts_per_tok
+        # 例如当tokens_per_expert=[6, 15, 20, 26, 33, 38, 46, 52]
+        # 当token_idxs=[3, 7, 19, 21, 24, 25,  4,  5,  6, 10, 11, 12...]
+        # 意味着当token_idxs[:6] -> [3,  7, 19, 21, 24, 25,  4]位置的token都由专家0处理，token_idxs[6:15]位置的token都由专家1处理......
+        for i, end_idx in enumerate(tokens_per_expert):
+            start_idx = 0 if i == 0 else tokens_per_expert[i - 1]
+            if start_idx == end_idx:
+                continue
+            expert = self.experts[i]
+            exp_token_idx = token_idxs[start_idx:end_idx]
+            expert_tokens = x[exp_token_idx]
+            expert_out = expert(expert_tokens).to(expert_cache.dtype)
+            expert_out.mul_(flat_expert_weights[idxs[start_idx:end_idx]])
+            # 使用 scatter_add_ 进行 sum 操作
+            expert_cache.scatter_add_(
+                0, exp_token_idx.view(-1, 1).repeat(1, x.shape[-1]), expert_out
+            )
+
+        return expert_cache
+
+
+class MiniMindBlock(nn.Module):
+    def __init__(self, layer_id: int, config: LMConfig):
+        super().__init__()
+        self.n_heads = config.n_heads
+        self.dim = config.dim
+        self.head_dim = config.dim // config.n_heads
+        self.attention = Attention(config)
+
+        self.layer_id = layer_id
+        self.attention_norm = RMSNorm(config.dim, eps=config.norm_eps)
+        self.ffn_norm = RMSNorm(config.dim, eps=config.norm_eps)
+        self.feed_forward = (
+            FeedForward(config) if not config.use_moe else MOEFeedForward(config)
+        )
+
+    def forward(self, x, pos_cis, past_key_value=None, use_cache=False):
+        h_attn, past_kv = self.attention(
+            self.attention_norm(x),
+            pos_cis,
+            past_key_value=past_key_value,
+            use_cache=use_cache,
+        )
+        h = x + h_attn
+        out = h + self.feed_forward(self.ffn_norm(h))
+        return out, past_kv
+
+
+class MiniMindLM(PreTrainedModel):
+    config_class = LMConfig
+
+    def __init__(self, params: LMConfig = None):
+        self.params = params or LMConfig()
+        super().__init__(self.params)
+        self.vocab_size, self.n_layers = params.vocab_size, params.n_layers
+        self.tok_embeddings = nn.Embedding(params.vocab_size, params.dim)
+        self.dropout = nn.Dropout(params.dropout)
+        self.layers = nn.ModuleList(
+            [MiniMindBlock(l, params) for l in range(self.n_layers)]
+        )
+        self.norm = RMSNorm(params.dim, eps=params.norm_eps)
+        self.output = nn.Linear(params.dim, params.vocab_size, bias=False)
+        self.tok_embeddings.weight = self.output.weight
+        self.register_buffer(
+            "pos_cis",
+            precompute_pos_cis(
+                params.dim // params.n_heads,
+                params.max_seq_len,
+                theta=params.rope_theta,
+            ),
+            persistent=False,
+        )
+        self.OUT = CausalLMOutputWithPast()
+
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        past_key_values: Optional[
+            List[Tuple[torch.Tensor, torch.Tensor]]
+        ] = None,
+        use_cache: bool = False,
+        **args,
+    ):
+        past_key_values = past_key_values or [None] * len(self.layers)
+        start_pos = args.get("start_pos", 0)
+        h = self.dropout(self.tok_embeddings(input_ids))
+        pos_cis = self.pos_cis[start_pos : start_pos + input_ids.size(1)]
+        past_kvs = []
+        for l_id, layer in enumerate(self.layers):
+            h, past_kv = layer(
+                h,
+                pos_cis,
+                past_key_value=past_key_values[l_id],
+                use_cache=use_cache,
+            )
+            past_kvs.append(past_kv)
+        logits = self.output(self.norm(h))
+        aux_loss = sum(
+            lay.feed_forward.aux_loss
+            for lay in self.layers
+            if isinstance(lay.feed_forward, MOEFeedForward)
+        )
+        self.OUT.__setitem__("logits", logits)
+        self.OUT.__setitem__("aux_loss", aux_loss)
+        self.OUT.__setitem__("past_key_values", past_kvs)
+        return self.OUT
+
+    @torch.inference_mode()
+    def generate(
+        self,
+        input_ids,
+        eos_token_id=2,
+        max_new_tokens=1024,
+        temperature=0.75,
+        top_p=0.90,
+        stream=False,
+        rp=1.0,
+        use_cache=True,
+        pad_token_id=0,
+        **args,
+    ):
+        # 流式生成
+        if stream:
+            return self._generate_stream(
+                input_ids,
+                eos_token_id,
+                max_new_tokens,
+                temperature,
+                top_p,
+                rp,
+                use_cache,
+            )
+
+        # 直接生成
+        generated = []
+        for i in range(input_ids.size(0)):
+            non_pad = input_ids[i][input_ids[i] != pad_token_id].unsqueeze(0)
+            out = self._generate_stream(
+                non_pad,
+                eos_token_id,
+                max_new_tokens,
+                temperature,
+                top_p,
+                rp,
+                use_cache,
+            )
+            tokens_list = [tokens[:, -1:] for tokens in out]
+            gen = torch.cat(tokens_list, dim=-1) if tokens_list else non_pad
+            full_sequence = torch.cat([non_pad, gen], dim=-1)
+            generated.append(full_sequence)
+        max_length = max(seq.size(1) for seq in generated)
+        generated = [
+            torch.cat(
+                [
+                    seq,
+                    torch.full(
+                        (1, max_length - seq.size(1)),
+                        pad_token_id,
+                        dtype=seq.dtype,
+                        device=seq.device,
+                    ),
+                ],
+                dim=-1,
+            )
+            for seq in generated
+        ]
+        return torch.cat(generated, dim=0)
+
+    def _generate_stream(
+        self,
+        input_ids,
+        eos_token_id,
+        max_new_tokens,
+        temperature,
+        top_p,
+        rp,
+        use_cache,
+        **args,
+    ):
+        start, first_seq, past_kvs = input_ids.shape[1], True, None
+        while input_ids.shape[1] < max_new_tokens - 1:
+            if first_seq or not use_cache:
+                out, first_seq = (
+                    self(
+                        input_ids, past_key_values=past_kvs, use_cache=use_cache
+                    ),
+                    False,
+                )
+            else:
+                out = self(
+                    input_ids[:, -1:],
+                    past_key_values=past_kvs,
+                    use_cache=use_cache,
+                    start_pos=input_ids.shape[1] - 1,
+                )
+            logits, past_kvs = out.logits[:, -1, :], out.past_key_values
+            logits[:, list(set(input_ids.tolist()[0]))] /= rp
+            logits /= temperature + 1e-9
+            if top_p is not None and top_p < 1.0:
+                sorted_logits, sorted_indices = torch.sort(
+                    logits, descending=True, dim=-1
+                )
+                sorted_probs = F.softmax(sorted_logits, dim=-1)
+                cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[:, 1:] = sorted_indices_to_remove[
+                    :, :-1
+                ].clone()
+                sorted_indices_to_remove[:, 0] = False
+                indices_to_remove = sorted_indices_to_remove.scatter(
+                    1, sorted_indices, sorted_indices_to_remove
+                )
+                logits[indices_to_remove] = -float("Inf")
+            input_ids_next = torch.multinomial(
+                F.softmax(logits, dim=-1), num_samples=1
+            )
+            input_ids = torch.cat((input_ids, input_ids_next), dim=1)
+            yield input_ids[:, start:]
+            if input_ids_next.item() == eos_token_id:
+                break
