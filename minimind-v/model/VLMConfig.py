78f9b1b228223693ea2f19da7cf94bda2bea0c25
XavierZXY <zxy_xavier@163.com>
2025-03-26 19:19:42 +0800

    feat: :sparkles: Add VLMConfig class for MiniMind-V model configuration

---
+++ minimind-v/model/VLMConfig.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from .LMConfig import LMConfig
+
+
+class VLMConfig(LMConfig):
+    model_type = "minimind-v"
+
+    def __init__(
+        self,
+        image_special_token: str = "@" * 196,
+        image_ids: List = [34] * 196,
+        **kwargs,
+    ):
+        self.image_special_token = image_special_token
+        self.image_ids = image_ids
+        super().__init__(**kwargs)
