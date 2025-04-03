a7a08822f5f71732229612ca558e083f37a4e2cd
XavierZXY <zxy_xavier@163.com>
2025-04-03 21:58:59 +0800

    feat: :sparkles: Add VisionProj class for vision projection in model_vlm.py

--- minimind-v/model/model_vlm.py
+++ minimind-v/model/model_vlm.py
@@ -1,9 +1,23 @@
-from .VLMConfig import VLMConfig
-from .model import *
-from typing import Optional, Tuple, List
-from torch import nn
 import warnings
-from transformers import CLIPProcessor, CLIPModel
-import torch
+from typing import List, Optional, Tuple
 
-warnings.filterwarnings('ignore')
+import torch
+from torch import nn
+from transformers import CLIPModel, CLIPProcessor
+
+from .model import *
+from .VLMConfig import VLMConfig
+
+warnings.filterwarnings("ignore")
+
+
+class VisionProj(nn.Module):
+    def __init__(self, ve_dim=768, lm_dim=512):
+        super().__init__()
+        self.ve_dim = ve_dim
+        self.lm_dim = lm_dim
+        self.vision_proj = nn.Sequential(nn.Linear(self.ve_dim, self.lm_dim))
+
+    def forward(self, image_encoders):
+        vision_proj = self.vision_proj(image_encoders)
+        return vision_proj
