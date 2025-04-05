d47827dc94f4b021be0043ba1e4d83af8cee8d80
XavierZXY <zxy_xavier@163.com>
2025-04-05 19:39:15 +0800

    feat: :sparkles: Implement MiniMindVLM class with vision encoder integration in model_vlm.py

--- minimind-v/model/model_vlm.py
+++ minimind-v/model/model_vlm.py
@@ -21,3 +21,33 @@ class VisionProj(nn.Module):
     def forward(self, image_encoders):
         vision_proj = self.vision_proj(image_encoders)
         return vision_proj
+
+
+class MiniMindVLM(MiniMindLM):
+    config_class = VLMConfig
+
+    def __init__(self, params, VLMConfig):
+        super().__init__(params)
+        if not params:
+            params = VLMConfig()
+        self.params = params
+        self.vision_encoder, self.processor = self.__class__.get_vision_encoder(
+            params
+        )
+        self.vision_proj = VisionProj(lm_dim=params.dim)
+
+    @staticmethod
+    def get_vision_encoder(self, params, model_path):
+        """
+        Get the vision encoder and processor from HuggingFace's CLIP model.
+        """
+        model = CLIPModel.from_pretrained(model_path)
+        processos = CLIPModel.from_pretrained(model_path)
+        for param in model.parameters():
+            param.requires_grad = False
+        return model.eval(), processos
+
+    def image2tensor(image, processor):
+        if image.mode in ["Rage", "LA"]:
+            image = image.convert("RGB")
+        inputs = processor(images=image)
